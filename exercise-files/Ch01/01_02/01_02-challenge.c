27921e3bba7be60baa74c044a28948fe1d2b7534
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-10 17:26:03 -0600

    The Fundamentals of C: challenge

---
+++ exercise-files/Ch01/01_02/01_02-challenge.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	puts("I solved the challenge.");
+	return 0;
+}
---
+++ exercise-files/Ch01/01_02/01_02-challenge2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	puts("With this output");
+	puts("I have solved the bonus challenge");
+	return 0;
+}
