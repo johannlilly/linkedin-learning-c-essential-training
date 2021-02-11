23654d70be5f866cc955245d569cafd9ccc7aa05
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-11 11:03:18 -0600

    Variables and Values: Challenge: make variables and constants
    Because B is a constant and only used in this file, I used the define directive

--- exercise-files/Ch02/02_06/02_06-challenge1.c
+++ exercise-files/Ch02/02_06/02_06-challenge1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define B 'B'
+
 int main()
 {
+	int a = 1;
+
 	printf("Here is the value of A: %d\n",a);
-	printf("Here is the value of B: %c\n",b);
+	printf("Here is the value of B: %c\n",B);
 
 	return(0);
 }
